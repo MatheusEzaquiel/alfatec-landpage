1d36608a88bca34c0b84cf0f42af7c13b0254ef5
matheusezaquiel <matheusbezaquiel@gmail.com>
2025-03-22 23:34:04 -0300

    refactor: Alteração dos Links do Menu mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,9 @@ import { useState } from "react";
 
 export default function Header() {
     const menuItem = [
-        { id: 1, name: "Inicio", link: "/", value: "home"},
-        { id: 2, name: "Sobre", link: "/sobre", value: "about" },
-        { id: 3, name: "Cursos", link: "/cursos", value: "course" },
+        { id: 1, name: "Inicio", link: "/", value: "home" },
+        { id: 2, name: "Cursos", link: "/cursos", value: "course" },
+        { id: 3, name: "Sobre", link: "/sobre", value: "about" },
         { id: 4, name: "Contato", link: "/contato", value: "contact" },
     ];
 
@@ -20,7 +20,7 @@ export default function Header() {
             <h1 className="text-zinc-50 text-4xl font-extrabold font-serif absolute left-1/2 transform -translate-x-1/2 md:static md:transform-none">
                 <Link to="/">Alfatec</Link>
             </h1>
-            
+
             <div className="md:hidden">
                 <Menu onClick={handleOpenMenu} />
             </div>
@@ -36,7 +36,7 @@ export default function Header() {
                     </a>
                 ))}
             </nav>
-            
+
             <div
                 className={`fixed top-0 right-0 h-full w-64 bg-orange-700/80 z-50 transform transition-transform duration-300 ease-in-out rounded-tl-[50px] rounded-bl-[50px] 
                             ${open ? "translate-x-0" : "translate-x-full"} md:hidden`}
@@ -47,6 +47,16 @@ export default function Header() {
                 <nav className="flex flex-col items-start mt-20 space-y-6 px-6">
                     {menuItem.map((item) => (
                         <div key={item.id} className="flex flex-col space-y-4 w-full text-center">
+                            <a href={`#${item.value}`}
+                                className="text-white text-2xl m-auto hover:underline"
+                                onClick={handleOpenMenu}
+                            >
+                                {item.name}
+                            </a>
+                            <div className="w-full border-t-[1px] border-white"></div>
+                        </div>
+                        /*
+                        <div key={item.id} className="flex flex-col space-y-4 w-full text-center">
                             <Link
                                 to={item.link}
                                 className="text-white text-2xl m-auto hover:underline"
@@ -56,6 +66,7 @@ export default function Header() {
                             </Link>
                             <div className="w-full border-t-[1px] border-white"></div>
                         </div>
+                        */
                     ))}
                     <p className="fixed bottom-8 left-0 right-0 text-center text-white">
                         &copy; Rede de Ensino Alfatec - 2024
